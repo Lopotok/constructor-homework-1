4bb44950441406ef06eaa99c2602e1eb9ddb0373
Bonivárt Krisztián <krisztian.bonivart@gmail.com>
2023-06-09 04:42:44 +0200

    2.b Írd át a példányosítást, hogy az új konstuktort használja! 1.BankAccount account = new BankAccount("66642069"); 2. Main-be : bankAccount átírása = account 3. //      account.setAccountNumber("123-456-789");

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-       BankAccount bankAccount= new BankAccount();
+       BankAccount account = new BankAccount("66642069");
        BankAccount bankAccount1= new BankAccount();
 
-      bankAccount.setAccountNumber("123-456-789");
-      bankAccount.setAccountHolder("Customer_One");
-      bankAccount.setBalace(112233);
+//      account.setAccountNumber("123-456-789");
+      account.setAccountHolder("Customer_One");
+      account.setBalace(112233);
 
-        System.out.println(" Az elso ugyfel szamlaszama: " + bankAccount.getAccountNumber()+
-                "\n A számlatulajdonos neve: "+ bankAccount.getAccountHolder()+
-                        "\n Egyenlege: " +bankAccount.getBalance());
+        System.out.println(" Az elso ugyfel szamlaszama: " + account.getAccountNumber()+
+                "\n A számlatulajdonos neve: "+ account.getAccountHolder()+
+                        "\n Egyenlege: " +account.getBalance());
 
       bankAccount1.setAccountNumber("987-654-321");
       bankAccount1.setAccountHolder("Customer_two");
