19a086c03a3b5b8b231fab01314b190de2ce38b6
Bonivárt Krisztián <krisztian.bonivart@gmail.com>
2023-06-09 04:10:45 +0200

    Hazi 1.a, 1.b,1.c

--- BankAccount.java
+++ BankAccount.java
@@ -1,3 +1,28 @@
 public class BankAccount {
-    // TODO
+    // TODO 1.a Fieldek
+    //Készíts egy BankAccount osztályt, az alábbi privát fieldekkel:
+    // accountNumber (String), accountHolder (String) és balance (double)!
+
+    private String accountNumber;
+    private String accountHolder;
+    private double balance;
+
+    public void setAccountNumber( String accountNumber){
+        this.accountNumber=accountNumber;
+    }
+    public String getAccountNumber(){
+        return accountNumber;
+    }
+    public void setAccountHolder(String accountHolder){
+        this.accountHolder=accountHolder;
+    }
+    public String getAccountHolder(){
+        return accountHolder;
+    }
+    public void setBalace(double balance){
+        this.balance=balance;
+    }
+    public double getBalance() {
+        return balance;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+
+        BankAccount bankAccount= new BankAccount();
+        BankAccount bankAccount1= new BankAccount();
+
+      bankAccount.setAccountNumber("123-456-789");
+      bankAccount.setAccountHolder("Customer_One");
+      bankAccount.setBalace(112233);
+
+        System.out.println(" Az elso ugyfel szamlaszama: " + bankAccount.getAccountNumber()+
+                "\n A számlatulajdonos neve: "+ bankAccount.getAccountHolder()+
+                        "\n Egyenlege: " +bankAccount.getBalance());
+
+      bankAccount1.setAccountNumber("987-654-321");
+      bankAccount1.setAccountHolder("Customer_two");
+      bankAccount1.setBalace(332211);
+
+        System.out.println(" Az masodik ugyfel szamlaszama: " + bankAccount1.getAccountNumber()+
+                "\n A számlatulajdonos neve: "+ bankAccount1.getAccountHolder()+
+                "\n Egyenlege: " +bankAccount1.getBalance());
+
         Car myCar = new Car("Toyota", "Camry", 2022);
         System.out.println(myCar.getBrand()); // Output: Toyota
 
